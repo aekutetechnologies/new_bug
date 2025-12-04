ffa5f5d7479c13f6253910100d76bb57e72fc7df
Smruti Sahoo <smrutisahoo@Smrutis-Mac-mini.local>
2025-12-05 00:17:36 +0530

    First commit

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -78,13 +78,28 @@ TEMPLATES = [
 WSGI_APPLICATION = 'config.wsgi.application'
 
 # Database
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+DB_NAME = os.getenv('DB_NAME', 'bugbear')
+DB_USER = os.getenv('DB_USER', 'bugbear')
+DB_PASSWORD = os.getenv('DB_PASSWORD', 'bugbear')
+DB_HOST = os.getenv('DB_HOST', 'localhost')
+DB_PORT = os.getenv('DB_PORT', '5432')
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': DB_NAME,
+        'USER': DB_USER,
+        'PASSWORD': DB_PASSWORD,
+        'HOST': DB_HOST,
+        'PORT': DB_PORT,
     }
 }
-
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
     {
